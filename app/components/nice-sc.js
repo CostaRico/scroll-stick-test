4b219e8ea603fddc50006f0677c1dcf475e8d593
Costa <Zetogo@gmail.com>
2016-04-24 12:10:18 +0300

    ok

--- app/components/nice-sc.js
+++ app/components/nice-sc.js
@@ -1,17 +1,24 @@
 import Ember from 'ember';
+import _ from 'underscore';
+function l(e) {
+  console.log(e);
+}
 
 export default Ember.Component.extend({
+  classNames: ['nice-sc'],
+  heights: [],
+  baseItem: 0,
   data: function () {
     var res = [];
-    for (var g = 0; g < 20; g++) {
+    for (var g = 0; g < 300; g++) {
       var group = {
         data: {
-          name: 'name'+g,
-          id: 'id#'+g
+          name: 'name' + g,
+          id: 'id#' + g
         },
         items: []
       };
-      for (var i = 0; i < 30; i++) {
+      for (var i = 0; i < _.random(2, 50); i++) {
         group.items.push({
           name: 'name' + i,
           lastName: 'lastName' + i,
@@ -22,42 +29,109 @@ export default Ember.Component.extend({
     }
     return res;
   }.property(),
-  onScroll(ev){
-    Em.run.debounce(this, ()=>{
-      var scrollVal  =ev.target.scrollTop;
-      var summ = 0;
-      this.$('div.group').each((i, item)=>{
-        //var itemOffset = item.offsetTop;//отступ сверху контейнера
-        var $item = $(item);
-        var itemHeight = $item.height();
 
-        summ += itemHeight;
+  visible: function () {
+    var bi = this.get('baseItem');
+    //l('Render items: '+(0+cI)+' - '+ (cI+10));
+    //return this.get('data').slice(0+cI, cI+5);
+    return this.get('data').slice(0+bi, 10+bi);
+  }.property('data', 'baseItem'),
 
-        if(scrollVal <  summ && 0 < (scrollVal-itemHeight)){
-          $('div.group-info').removeClass('curr');
-          $('div.group').css('padding-top', '0');
 
-          $(item).find('.group-info').addClass('curr');
-          $(item).css('padding-top', '39px;');
-          console.log('======================================');
-          console.log('index: '+i);
-          console.log('Высота элемента:'+itemHeight);
-          console.log('Сумма высот элементов:'+summ);
-          console.log('Прокручено'+scrollVal);
-          console.log('======================================');
-          return false;
-        }
+  elHeight: 50,
+  getActiveItem: function (currST) {
+    var r = 1;
+   // l(this.offsets);
+
+    var summ = 0;
+    for (var i = 0; i < this.heights.length; i++) {
+      var currH = this.heights[i];
+      var prevEl = 0;
+      if(this.heights[i-1]){
+        prevEl = this.heights[i-1];
+      }
+
+      summ = summ+this.heights[i];
+      //l('CurrST: '+currST + ' Prev height: ' + prevEl + ' CurrHeight: ' + currH);
+      if(currST >= (summ-currH) && currST < summ){
+        r = i;
+        break;
+      }
+    }
+    return r+1;
+  },
+  calcHeights(){
+    this.heights = [];
+    this.$('.group').each((i, el)=> {
+      //this.offsets.push($(el).position().top);
+      this.heights.push($(el).outerHeight());
+    });
+  },
+  setLimits(AI){
+    var bi = this.get('baseItem');
+    if(AI==8 && this.lastAI < 8){
+      this.set('baseItem', bi+3);
+      Ember.run.next(()=>{
+        this.calcHeights();
+        var newMargin = 0;
+        this.$('.group').slice(0,4).each((i, el)=> {
+          newMargin = newMargin + $(el).height();
+        });
+        this.get('cont').scrollTop( newMargin );
       });
+    }
 
-      //console.log('==================');
-      //console.log($(ev.target).offset().top);
-    }, 150, true);
+    if(AI==2 && this.lastAI>2){
+      if(bi-2 >= 0){
+        this.set('baseItem', bi-2);
+        Ember.run.next(()=>{
+          this.calcHeights();
+          var newMargin = 0;
+          this.$('.group').slice(0,4).each((i, el)=> {
+            newMargin = newMargin + $(el).height();
+          });
+          this.get('cont').scrollTop( newMargin );
+        });
+      }else{
+        this.set('baseItem', 0);
+      }
+
+
+    }
   },
   didInsertElement(){
-    this.$('.sc-container').scroll((ev)=>{
-      this.onScroll(ev);
-    });
-    //window.addEventListener('scroll', this.onScroll, false);
+    this._super(arguments);
+    this.set('cont', this.$('.sc-container'));
+    this.calcHeights();
+
+
+    this.get('cont').scroll((ev)=> {
+
+      Em.run.debounce(this, ()=> {
+        this.onScroll(ev);
+      }, 30, true);
+
+    }).trigger('scroll');
+  },
+
+  elMargin: '0px',
+  scrollTop: 0,
+  lastAI: false,
+  onScroll(ev){
+    var AI = this.getActiveItem(ev.target.scrollTop);
+    this.scrollTop = ev.target.scrollTop;
+    //l('Current AI: '+AI );
+    if(this.lastAI!=AI){
+      this.$('div.group div.group-info').removeClass('curr');
+      this.$('div.group').eq(AI-1).find('div.group-info').addClass('curr');
+    }
+    if(AI==2 || AI ==8 ){
+      //rerender items
+      this.setLimits(AI);
+      //resetAI
+    }
+    this.lastAI = AI;
+
   }
 
 });
